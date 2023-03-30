83aea3ab392599349dd7dd0548f8884976e993d0
Andrew Benson <abenson@obs.carnegiescience.edu>
2023-03-30 08:00:49 -0700

    fix: Change copyright

--- galacticusInstall.sh
+++ galacticusInstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Galacticus install script.
-# © Andrew Benson 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
+# Copyright Andrew Benson 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
 
 # Functions
 function contains() {
