0cdec74338f5d64619444d91c774a95411698825
Andrew Benson <abenson@obs.carnegiescience.edu>
2026-02-22 11:42:04 -0800

    fix: Update `zlib` to version 1.3.2

--- galacticusInstall.sh
+++ galacticusInstall.sh
@@ -625,7 +625,7 @@ iPackage=$(expr $iPackage + 1)
       maxVersion[$iPackage]="9.9.9"
       yumInstall[$iPackage]="zlib-devel"
       aptInstall[$iPackage]="zlib1g-dev"
-       sourceURL[$iPackage]="http://zlib.net/zlib-1.3.1.tar.gz"
+       sourceURL[$iPackage]="http://zlib.net/zlib-1.3.2.tar.gz"
 buildEnvironment[$iPackage]=""
    buildInOwnDir[$iPackage]=0
    configOptions[$iPackage]="--prefix=$toolInstallPath"
